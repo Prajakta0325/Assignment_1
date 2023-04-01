a6ddf66c9959248d634fd4d5f6a2b5481fdbeaba
Prajakta0325 <129607388+Prajakta0325@users.noreply.github.com>
2023-04-01 20:35:43 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,33 @@
+#integer
+#a=10
+#print(type(a))
+
+#float
+#a=10.5
+#print(type(a))
+
+#str
+a= 'Prajakta'
+print(type(a))
+print(a)
+
+#List
+#a= [1,2,3]
+#print(type(a))
+#print(a)
+
+"""Set
+a= {1,2,3,4,5,1}
+print(type(a))
+print(a)"""
+
+#tuple
+#a= (1,2,3,)
+#print(type(a))
+
+#dict
+#a= {"name":"Praju","age":"25"}
+#print(type(a))
+#print(a)
+
+#boolean true flase
